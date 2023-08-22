52a7d74221ac572f8ef6a38ba96b086898760506
Shreshth Pal <shreshthpal@outlook.com>
2023-08-22 09:10:11 +0530

    Central error manangement added

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 	"text/template"
@@ -10,7 +9,7 @@ import (
 
 
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -22,13 +21,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	ts,err := template.ParseFiles(files...)
 	if err != nil {
-		fmt.Fprint(w,"Internal Server Error",http.StatusInternalServerError)
+		app.serverError(w,err)
 		return
 	}
 	err = ts.ExecuteTemplate(w,"base",nil)
 	if err != nil {
-		log.Print(err,"error")
-		http.Error(w,"Internal Server Error",http.StatusInternalServerError)
+		app.serverError(w,err)
 		return
 	}
 
@@ -37,23 +35,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
-id, err := strconv.Atoi(r.URL.Query().Get("id"))
-if err != nil || id < 1 {
-http.NotFound(w, r)
-return
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
-fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
 
 
 
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-if r.Method != http.MethodPost {
-w.Header().Set("Allow", http.MethodPost)
-http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-return
-}
-w.Write([]byte("Create a new snippet..."))
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
 }
