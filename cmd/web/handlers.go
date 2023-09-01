3c15cb4018f9bf95945aac2d0cdbad07d0f16b10
Shreshth Pal <shreshthpal@outlook.com>
2023-09-01 13:27:33 +0530

    Dynamic HTML and template updates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,65 +9,47 @@ import (
 	"Snippetbox.Shreshth/internal/models"
 )
 
-
-
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+	app.notFound(w)
+	return
 	}
 	snippets, err := app.snippets.Latest()
+	
+	
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
-	}
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+	// Use the new render helper.
+	app.render(w, http.StatusOK, "home.tmpl", data)
 
 
-
-
-	// files := []string {
-	// 	"./ui/html/base.tmpl.html",
-	// 	"./ui/html/pages/home.tmpl.html",
-	// 	"./ui/html/partials/nav.tmpl.html",
-	// }
-	// ts,err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w,err)
-	// 	return
-	// }
-	// err = ts.ExecuteTemplate(w,"base",nil)
-	// if err != nil {
-	// 	app.serverError(w,err)
-	// 	return
-	// }
-
-	
 }
-
-
-
-func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		app.notFound(w)
-		return
+	app.notFound(w)
+	return
 	}
 	snippet, err := app.snippets.Get(id)
 	if err != nil {
-		if errors.Is(err, models.ErrNoRecord) {
-			app.notFound(w)
-		} else {
-		app.serverError(w, err)
-		}
-		return
+	if errors.Is(err, models.ErrNoRecord) {
+	app.notFound(w)
+	} else {
+	app.serverError(w, err)
 	}
-
-	fmt.Fprintf(w, "%+v", snippet)
+	return
+	}
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+	app.render(w, http.StatusOK, "view.tmpl", data)
 }
+
+
 
 
 
@@ -91,3 +73,4 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	// Redirect the user to the relevant page for the snippet.
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
+
