3567822b912192e9f03c5d8803e9ba6e1d32aeeb
Shreshth Pal <shreshthpal@outlook.com>
2023-09-03 22:49:06 +0530

    Added Form and Validators

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"Snippetbox.Shreshth/internal/models"
+	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -18,6 +19,7 @@ type application struct {
 	infoLog *log.Logger
 	snippets *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder *form.Decoder
 }
 
 
@@ -41,11 +43,15 @@ func main() {
 	if err != nil {
 		errorLog.Fatal(err)
 	}
+
+	formDecoder := form.NewDecoder()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
 		snippets: &models.SnippetModel{DB:db},
 		templateCache: templateCache,
+		formDecoder: formDecoder,
 	}
 	srv := &http.Server{
 		Addr: *addr,
